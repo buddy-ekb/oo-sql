d364dc81b45cb92a2a488533b4690f59af65642e
Andrey Alcheev <buddy@k66.ru>
2017-05-22 14:52:29 +0500

    An instance distinguisher in "open_new_window" functionality

--- gui.open_new_window.sql
+++ gui.open_new_window.sql
@@ -3,9 +3,16 @@ CREATE OR REPLACE FUNCTION gui.open_new_window(p_username text, p_viewid integer
  LANGUAGE plpgsql
 AS $function$
 DECLARE
-    v_payload text;
+    v_payload TEXT;
+    v_instance TEXT;
 BEGIN
     v_payload := COALESCE(p_username, '') || ',' || p_viewid::text || ',' || COALESCE(p_x::text, '') || ',' || COALESCE(p_y::text, '') || ',' || COALESCE(p_width::text, '') || ',' || COALESCE(p_height::text, '') || ',' || COALESCE(p_id, '') || ',' || COALESCE(p_colname, '');
+    BEGIN
+        SELECT current_setting('session.instance') INTO v_instance;
+        v_payload := v_payload || ',' || v_instance;
+    EXCEPTION WHEN OTHERS THEN
+        -- do nothing
+    END;
     EXECUTE 'NOTIFY _open_new_window_, ''' || v_payload || '''';
 END
 $function$
